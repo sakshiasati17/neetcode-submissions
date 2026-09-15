a6fd4e55200824c5535ff9ea6581d75fce75d7df
Sakshi Asati <sakshiasati51@gmail.com>
2026-09-15 14:05:25 -0700

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        sol = []
+
+        def backtrack(i):
+            if i == len(nums):
+                res.append(sol[:])
+                return
+
+            # Decision 1: Skip nums[i]
+            backtrack(i + 1)
+
+            # Decision 2: Take nums[i]
+            sol.append(nums[i])
+            backtrack(i + 1)
+            sol.pop()
+
+        backtrack(0)
+        return res
